fd975976c1aca26e9b700c8751091075f1dfb246
victorrodegherosantos-coder <victor.rodeghero.santos@escola.pr.gov.br>
2025-11-14 10:40:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+const titulo=document.getElementById("titulo");
+const op1=document.getElementById("op1");
+const op2=document.getElementById("op2");
+const texto=document.getElementById("texto");
+
+const passos=["Um dia desses, acordei com meu avô querendo me liberar da prisão, me dizendo que estava em busca da cura da doença da minha mãe, plausível apenas em território asiático.",
+"Na Índia fomos interceptados por um grupo de criminosos que disseminavam a doença mortal da minha mãe.",
+"Ao chegarmos na China, fomos recebidos por um velho amigo de meu avô que nos instruiu a chegar em um curandeiro local.",
+"Os mercenários indicam seu inimigo mortal, o curandeiro, que vive na China.",
+"Você permite que a sua mãe, decrépita com idade e a doença, descanse em paz.",
+"O curandeiro oferece seus serviços se o dar as presas de um vampiro.",
+"Ao encontrar o vampiro, você o espanca e leva de volta suas presas ao curandeiro",
+"Em uma adega, você encontra um caçador de vampiros que te indica o Egito.",
+"Com o remédio em mãos, você volta para casa e salva sua mãe da doença."
+];
+const opcoes=[
+"",
+"Ir aos portos para a Índia",
+"Pegar um avião à China",
+"Derrotar e Interrogá-los",
+"Desistir e voltar para casa",
+"Ir ao curandeiro",
+"Ir ao Egito",
+"Ir à Europa",
+"Voltar ao curandeiro e o dar as presas do vampiro"
+];
